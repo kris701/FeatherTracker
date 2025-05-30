427187986594983d079fabbbef1362d92a13cadc
Kristian Skov <kris701kj@gmail.com>
2025-05-30 07:50:00 +0200

    Updated gitignore

--- Frontend/src/app/pages/platform/pages/dashboard/dashboard.ts
+++ Frontend/src/app/pages/platform/pages/dashboard/dashboard.ts
@@ -8,6 +8,10 @@ import { CommonModule } from '@angular/common';
         <div class="card">
             Welcome to Feather Tracker!
         </div>
+        <div class="card">
+            <p>To start using feather tracker, navigate to the sidebar on the left side and click 'Add Bird'</p>
+            <p>When you have added your bird, you can click on the bird under the 'Weight Tracking' page and start entering weights!</p>
+        </div>
     `
 })
 export class Dashboard {
