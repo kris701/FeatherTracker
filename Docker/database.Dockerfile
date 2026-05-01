c9146014b8c7f257f44233123e6dd1130601becc
Kristian Skov <kris701kj@gmail.com>
2026-05-01 20:51:55 +0200

    Docker compose fixes

--- Docker/database.Dockerfile
+++ Docker/database.Dockerfile
@@ -2,4 +2,5 @@ FROM mcr.microsoft.com/azure-sql-edge
 
 ENV ACCEPT_EULA=1
 
-COPY ./Docker/Database/FeatherTracker.Database_Create.sql /docker-entrypoint-initdb.d/
+COPY ./Docker/Database/FeatherTracker.Database_Create.sql /databaseinit/
+COPY ./Docker/databaseinit.sh /databaseinit/
---
+++ Docker/databaseinit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+/opt/mssql/bin/sqlservr &
+
+for i in {1..5};
+do
+    # Check if Database already exists
+    RESULT=`/opt/mssql-tools/bin/sqlcmd -S docker-feathertracker-database -U sa -P 'yourStrong(!)Password' -Q "IF DB_ID('FeatherTracker.Database') IS NOT NULL print 'YES'"`
+    CODE=$?
+    
+    if [[ $RESULT == "YES" ]]
+    then
+        echo "Database already initialized."
+        break;
+    elif [[ $CODE -eq 0 ]] && [[ $RESULT == "" ]]
+    then
+        echo "Initializing database!"
+	/opt/mssql-tools/bin/sqlcmd -l 5 -S docker-feathertracker-database -U sa -P 'yourStrong(!)Password' -d master -i /docker-entrypoint-initdb.d/FeatherTracker.Database_Create.sql;
+        break;
+    else
+        echo "Database not ready yet..."
+        sleep 1
+    fi
+done
+
+sleep infinity;
